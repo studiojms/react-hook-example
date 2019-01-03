29da5e4ffa4456a273b4d83d11669261a822f852
Jefferson Mariano de Souza <jefferson.msouza86@gmail.com>
2019-01-03 15:10:33 -0200

    using sample hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
+import { useState } from 'react';
 import 'antd/dist/antd.css';
 
-import { Button } from 'antd';
+import { Button, Progress, Row, Col } from 'antd';
 
-export const App = () => (
-  <div>
-    <h1>Test</h1>
-    <Button>Click</Button>
-  </div>
-);
+export const App = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <h1>Value: {value}</h1>
+      <Row type="flex">
+        <Col span={10} offset={2}>
+          <Progress percent={value + 50} type="circle" />
+        </Col>
+      </Row>
+      <Row>
+        <Col span={6}>
+          <Button type="danger" onClick={() => setValue(value - 1)}>
+            -
+          </Button>
+        </Col>
+        <Col span={6}>
+          <Button type="primary" onClick={() => setValue(value + 1)}>
+            +
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  );
+};
